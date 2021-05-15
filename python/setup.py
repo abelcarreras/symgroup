c8b9446fa06378070c05d3d4c35e39129898371d
Abel Carreras <abelcarreras83@gmail.com>
2021-05-15 03:04:37 +0200

    Added windows support to setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,7 @@
 from numpy.distutils.core import setup, Extension
 from distutils.dir_util import copy_tree
 from distutils.errors import DistutilsFileError
-
-#import shutil
+import os, sys
 
 
 def get_version_number():
@@ -13,7 +12,6 @@ def get_version_number():
 
 
 # Make python package
-
 try:
     copy_tree('../src', './src', update=True)
 except DistutilsFileError:
@@ -21,10 +19,19 @@ except DistutilsFileError:
 
 s_dir = 'src/'
 
+# If windows
+if sys.platform.startswith('win'):
+    libraries = []
+    #libraries = ['lapack', 'refblas']
+    with open('setup.cfg', 'w') as f:
+        f.write('[build_ext]\ninplace=1\nlibraries=lapack,refblas')
+else:
+    libraries=['lapack', 'blas'],
+
 symgroupy = Extension('symgroupy.symgrouplib',
-                      #include_dirs=include_dirs_numpy,
+                      # include_dirs=include_dirs_numpy,
                       extra_f77_compile_args=['-ffixed-line-length-0'],
-                      libraries=['lapack', 'blas'],
+                      libraries=libraries,
                       sources=['symgrouplib.pyf',
                                s_dir + 'symgrouplib.F',
                                s_dir + 'radius.F',
@@ -41,4 +48,16 @@ setup(name='symgroupy',
       author='Abel Carreras',
       author_email='abelcarreras83@gmail.com',
       packages=['symgroupy'],
-      ext_modules=[symgroupy])
+      package_data={"": ["*.dll"],},
+      include_package_data=True,
+      ext_modules=[symgroupy],
+      setup_cfg=True)
+
+# If windows
+if sys.platform.startswith('win'):
+    from shutil import copyfile
+    dir = os.path.dirname(__file__)
+    files = os.listdir(dir + '/symgroupy/.libs')
+    for file in files:
+        filename = os.path.join(dir, 'symgroupy','.libs', file)
+        copyfile(filename, os.path.join(dir, 'symgroupy', file))
