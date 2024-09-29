9d9888445c29cac74c08a4ac50c76c256f84744b
Abel Carreras <abelcarreras83@gmail.com>
2024-09-30 00:44:35 +0200

    update to meson backend to compile fortran extensions Add compatibility with numpy 2.x

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,11 @@
-from numpy.distutils.core import setup, Extension
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install
 from distutils.dir_util import copy_tree
 from distutils.errors import DistutilsFileError
-import sys
+import sys, os
+import subprocess
+import pathlib
 
 def get_version_number():
     main_ns = {}
@@ -11,7 +15,6 @@ def get_version_number():
             return main_ns['__version__']
 
 # Make python package
-
 try:
     copy_tree('../src', './src', update=True)
 except DistutilsFileError:
@@ -19,12 +22,41 @@ except DistutilsFileError:
 
 s_dir = 'src/'
 
-if sys.version_info[1] > 7:
-    pyf_file = 'symgrouplib.pyf'
-else:
-    pyf_file = 'symgrouplib_legacy.pyf'
+
+class MesonBuildExt(build_ext):
+    def run(self):
+        # make compilation dir
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        print('self.build_lib:', self.build_lib)
+
+        workdir = os.path.dirname(os.path.abspath(__file__))
+
+        install_dir = pathlib.Path(workdir, 'symgroupy')
+
+        subprocess.check_call(['meson', 'setup', self.build_temp, '--prefix', install_dir])
+        subprocess.check_call(['meson', 'compile', '-C', self.build_temp])
+        if '--inplace' in sys.argv:
+            subprocess.check_call(['meson', 'install', '-C', self.build_temp])
 
 
+class InstallWithBuildExt(install):
+    def run(self):
+
+        self.build_temp = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'build/temp')
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        install_dir = pathlib.Path(self.install_lib, 'symgroupy')
+
+        subprocess.check_call(['meson', 'setup', self.build_temp, '--prefix', install_dir])
+        subprocess.check_call(['meson', 'compile', '-C', self.build_temp])
+        subprocess.check_call(['meson', 'install', '-C', self.build_temp])
+
+
+"""
 symgroupy = Extension('symgroupy.symgrouplib',
                       #include_dirs=include_dirs_numpy,
                       extra_f77_compile_args=['-ffixed-line-length-0'],
@@ -38,6 +70,7 @@ symgroupy = Extension('symgroupy.symgrouplib',
                                s_dir + 'mass.F',
                                s_dir + 'measure.F',
                                s_dir + 'operations.F'])
+"""
 
 setup(name='symgroupy',
       version=get_version_number(),
@@ -47,7 +80,10 @@ setup(name='symgroupy',
       author='Abel Carreras',
       author_email='abelcarreras83@gmail.com',
       packages=['symgroupy'],
-      ext_modules=[symgroupy],
+      ext_modules=[],
+      cmdclass={'build_ext': MesonBuildExt,
+                'install': InstallWithBuildExt,
+                },
       url='https://github.com/abelcarreras/symgroup',
       classifiers=[
           "Programming Language :: Python",
