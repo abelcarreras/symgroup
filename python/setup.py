7643bbbce715c7a9fc5abcd853ef5fc5e3ade60f
Abel Carreras <abelcarreras83@gmail.com>
2024-11-20 14:25:15 +0100

    meson and setup cleaning

--- python/setup.py
+++ python/setup.py
@@ -29,55 +29,20 @@ except DistutilsFileError:
 s_dir = 'src/'
 
 
-from setuptools.command.egg_info import egg_info as _egg_info
-
-class CustomEggInfo(_egg_info):
-    """ Custom egg_info command to set the correct egg-info name and directory """
-
-    def run(self):
-        # Defineix el directori on es guardarà l'egg-info
-        egg_info_dir = os.path.join(os.getcwd(), 'build', 'egg-info')
-        egg_info_dir = '/Users/abel/Programes/symgroup/python/build/'
-        print('anterior', self.egg_base)
-
-        # self.distribution.package_dir = egg_info_dir
-
-        self.egg_base = egg_info_dir
-        self.egg_base = '/Users/abel/Programes/symgroup/python/build/bdist.macosx-10.9-universal2/'
-        print(f"Guardant egg-info a {egg_info_dir}")
-        #self.do_egg_install()
-
-        print('egg_base: ',self.egg_base)
-        print('egg_name: ',self.egg_name)
-        print('egg_info: ',self.egg_info)
-        print('egg_version: ',self.egg_version)
-        print('broken_egg_info: ', self.broken_egg_info)
-
-        self.egg_info = self.egg_base + 'egg/' + self.egg_name + '-' + self.egg_version + '-' + 'py3.11' + '.egg-info'
-        print('self.egg_info: ', self.egg_info)
-
-        self.egg_info = 'build/bdist.macosx-10.9-universal2/egg/symgroupy-0.5.12-py3.11.egg-info'
-
-        # ValueError: symgroupy-0.5.12-py3.11.egg-info but not found
-        # Crida a la funció base per generar l'egg-info correctament format
-        super().run()
-
-
 class MesonBuildExt(build_ext):
     def run(self):
-        # make compilation dir
+
+        # make compilation dir if needed
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
         print('self.build_lib:', self.build_lib)
 
+        # define module dir to place fortran extension
         workdir = os.path.dirname(os.path.abspath(__file__))
-
         install_dir = pathlib.Path(workdir, 'symgroupy')
-        # install_dir = os.path.abspath(install_dir)
 
-        #subprocess.check_call([sys.executable, 'setup.py', 'egg_info'])
-
+        # build with meson
         subprocess.check_call(['meson', 'setup', self.build_temp, '--prefix', install_dir])
         subprocess.check_call(['meson', 'compile', '-C', self.build_temp])
         if '--inplace' in sys.argv:
@@ -94,15 +59,11 @@ class InstallWithBuildExt(install):
 
         self.install_lib = self.install_lib.replace('purelib/', '')
 
+        # define module dir to install fortran extension
         install_dir = pathlib.Path(self.install_lib, 'symgroupy')
-        print('Install dir: ', self.install_lib, install_dir)
-        print('build dir:', self.build_temp)
         install_dir = os.path.abspath(install_dir)
 
-        print('version', self)
-        # subprocess.check_call([sys.executable, 'setup.py', 'egg_info', '--egg-base', self.install_lib])
-        #subprocess.check_call([sys.executable, 'setup.py', 'egg_info'])
-
+        # build with meson and install
         subprocess.check_call(['meson', 'setup', self.build_temp, '--prefix', install_dir])
         subprocess.check_call(['meson', 'compile', '-C', self.build_temp])
         subprocess.check_call(['meson', 'install', '-C', self.build_temp])
@@ -120,22 +81,17 @@ class MesonBdistWheel(_bdist_wheel):
         if not os.path.exists(self.dist_dir):
             os.makedirs(self.dist_dir)
 
+        # define project root dir
         workdir = os.path.dirname(os.path.abspath(__file__))
 
+        # define distribution dir
         dist_dir = pathlib.Path(workdir, self.dist_dir)
         dist_dir = os.path.abspath(dist_dir)
 
-        print('dist_dir: ', dist_dir)
-
+        # build with meson
         subprocess.check_call(['meson', 'setup', self.build_temp, '--prefix', dist_dir])
         subprocess.check_call(['meson', 'compile', '-C', self.build_temp])
-        # subprocess.check_call(['meson', 'install', '-C', self.build_temp])
 
-        # impl_tag, abi_tag, plat_tag = self.get_tag()
-        # archive_basename = "{}-{}-{}-{}".format(self.wheel_dist_name, impl_tag, abi_tag, plat_tag)
-        # print('archive_basename: ', archive_basename)
-
-        # self.skip_build = False
         self.root_is_pure = False
         super().run()
 
@@ -152,7 +108,6 @@ setup(name='symgroupy',
       cmdclass={'build_ext': MesonBuildExt,
                 'install': InstallWithBuildExt,
                 'bdist_wheel': MesonBdistWheel,
-                # 'egg_info': CustomEggInfo
                 },
       url='https://github.com/abelcarreras/symgroup',
       classifiers=[
